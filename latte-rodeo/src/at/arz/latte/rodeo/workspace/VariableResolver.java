d644493909b4dae60401ad6fe161b0123f781f5a
mrodler <mario.rodler@gmx.net>
2016-01-16 13:39:10 +0100

    added variable resolver

---
+++ latte-rodeo/src/at/arz/latte/rodeo/workspace/VariableResolver.java
@@ -0,0 +1,50 @@
+package at.arz.latte.rodeo.workspace;
+
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class VariableResolver {
+
+	private static final Pattern PROPERTY_PATTERN = Pattern.compile("(\\\\\\$)|\\$\\{([^\\\\$}]+)\\}");
+	private Properties properties;
+
+	public VariableResolver(Properties properties) {
+		this.properties = properties;
+	}
+
+	public String resolve(String value) {
+		Matcher matcher = PROPERTY_PATTERN.matcher(value);
+		final StringBuffer buffer = new StringBuffer();
+		int current = 0;
+		while (matcher.find()) {
+			buffer.append(value.substring(current, matcher.start()));
+			current = replace(matcher, buffer);
+		}
+		buffer.append(value.substring(current));
+		return buffer.toString();
+	}
+
+	private int replace(Matcher matcher, final StringBuffer buffer) {
+		String escape = matcher.group(1);
+		if (escape == null) {
+			appendVariable(buffer, matcher.group(2));
+		} else {
+			buffer.append("$");
+		}
+		return matcher.end();
+	}
+
+	private void appendVariable(final StringBuffer buffer, String key) {
+		String newValue = properties.getProperty(key);
+		if (newValue == null) {
+			buffer.append("${");
+			buffer.append(key);
+			buffer.append("}");
+		} else {
+			buffer.append(newValue);
+		}
+	}
+
+}
---
+++ latte-rodeo/test/at/arz/latte/rodeo/workspace/VariableResolverTest.java
@@ -0,0 +1,64 @@
+package at.arz.latte.rodeo.workspace;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class VariableResolverTest {
+
+	private VariableResolver resolver;
+
+	@Before
+	public void setup() {
+		Properties properties = new Properties();
+		properties.put("var1", "value1");
+		properties.put("var2", "value2");
+		resolver = new VariableResolver(properties);
+	}
+
+	@Test
+	public void must_return_original_value() {
+		assertThat(resolver.resolve("input"), is(("input")));
+	}
+
+	@Test
+	public void must_return_variable_only() {
+		assertThat(resolver.resolve("${var1}"), is("value1"));
+	}
+
+	@Test
+	public void must_return_variable_at_end() {
+		assertThat(resolver.resolve("${var1} end"), is("value1 end"));
+	}
+
+	@Test
+	public void must_return_variable_at_center() {
+		assertThat(resolver.resolve("begin ${var1} end"), is("begin value1 end"));
+	}
+
+	@Test
+	public void must_return_variable_at_begin() {
+		assertThat(resolver.resolve("${var1} end"), is("value1 end"));
+	}
+
+	@Test
+	public void is_not_recursive() {
+		assertThat(resolver.resolve("${${var1}}"), is("${value1}"));
+	}
+
+	@Test
+	public void replaces_multiple_vars() {
+		assertThat(resolver.resolve("${var1} ${var2}"), is("value1 value2"));
+	}
+
+	@Test
+	public void accepts_escape_char() {
+		assertThat(resolver.resolve("\\${var1}"), is("${var1}"));
+	}
+	
+}
