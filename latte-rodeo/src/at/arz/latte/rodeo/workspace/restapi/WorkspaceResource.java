12b79d6652565f2b20bc7280c34f9ec2a4d86968
mrodler <mario.rodler@gmx.net>
2016-01-18 14:25:49 +0100

    display directories in workspace. need file down and upload

--- latte-rodeo/src/at/arz/latte/rodeo/workspace/restapi/WorkspaceResource.java
+++ latte-rodeo/src/at/arz/latte/rodeo/workspace/restapi/WorkspaceResource.java
@@ -1,12 +1,14 @@
 package at.arz.latte.rodeo.workspace.restapi;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,6 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import at.arz.latte.rodeo.infrastructure.RodeoSecurity;
 import at.arz.latte.rodeo.workspace.Workspace;
 
 @Path("/workspaces")
@@ -22,6 +25,9 @@ public class WorkspaceResource {
 
 	@Inject
 	private Workspace workspace;
+
+	@Inject
+	RodeoSecurity security;
 
 	@Path("/")
 	@GET
@@ -45,6 +51,17 @@ public class WorkspaceResource {
 			}
 		}
 		return Response.status(Status.NOT_FOUND).type(MediaType.TEXT_PLAIN).entity(path + " not found").build();
+
+	}
+
+	@Path("{path}")
+	@PUT
+	public Response updateFile(@PathParam("path") String path, InputStream inputStream) {
+		security.assertUserIsAdmin();
+
+		// TODO: implement file update
+
+		return Response.status(Status.NOT_ACCEPTABLE).build();
 
 	}
 
