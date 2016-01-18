b2bded1a3ba2ec3aebdc9239d46c534e1b0d4148
Daniel Knapp <daniel.knapp@arz.at>
2016-01-18 18:55:45 +0100

    add Fileupload, Fileupdate, removing Files, creating and removing directories
    fix problems with path, now longer paths then one directory is possible

--- latte-rodeo/src/at/arz/latte/rodeo/workspace/restapi/WorkspaceResource.java
+++ latte-rodeo/src/at/arz/latte/rodeo/workspace/restapi/WorkspaceResource.java
@@ -1,13 +1,17 @@
 package at.arz.latte.rodeo.workspace.restapi;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,6 +19,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 
 import at.arz.latte.rodeo.infrastructure.RodeoSecurity;
 import at.arz.latte.rodeo.workspace.Workspace;
@@ -35,7 +41,7 @@ public class WorkspaceResource {
 		return getIndex("/");
 	}
 
-	@Path("{path}")
+	@Path("{path : .+}")
 	@GET
 	public Response getIndex(@PathParam("path") String path) {
 		File workspaceDir = workspace.getWorkspaceDir();
@@ -44,9 +50,8 @@ public class WorkspaceResource {
 			if (subdir.isDirectory()) {
 				return Response.status(Status.OK).entity(buildDirList(subdir)).build();
 			} else {
-				return Response.status(Status.OK)
-								.type(MediaType.TEXT_PLAIN)
-								.entity("i need to implement the file reader")
+				return Response.ok(subdir, MediaType.APPLICATION_OCTET_STREAM)
+								.header("Content-Disposition", "attachment; filename=\"" + subdir.getName() + "\"")
 								.build();
 			}
 		}
@@ -54,15 +59,97 @@ public class WorkspaceResource {
 
 	}
 
-	@Path("{path}")
+	@Path("{path : .+}")
 	@PUT
-	public Response updateFile(@PathParam("path") String path, InputStream inputStream) {
+	public Response uploadFile(@PathParam("path") String path, InputStream inputStream) {
 		security.assertUserIsAdmin();
 
-		// TODO: implement file update
+		File workspaceDir = workspace.getWorkspaceDir();
+		File subdir = new File(workspaceDir, path);
 
-		return Response.status(Status.NOT_ACCEPTABLE).build();
+		if (subdir.exists()) {
+			return Response.status(Status.CONFLICT).build();
+		}
 
+		try {
+			if (inputStream.available() == 0) {
+				subdir.mkdir();
+			} else {
+				subdir.createNewFile();
+
+				writeStreamToFile(inputStream, subdir);
+			}
+		} catch (IOException e) {
+			return Response.status(Status.NOT_ACCEPTABLE).build();
+		}
+
+		return Response.ok().build();
+	}
+
+	@Path("{path : .+}")
+	@POST
+	public Response updateFile(@PathParam("path") String path, InputStream inputStream) {
+		security.assertUserIsAdmin();
+		File workspaceDir = workspace.getWorkspaceDir();
+		File subdir = new File(workspaceDir, path);
+
+		if (!subdir.exists()) {
+			Response.status(Status.NOT_FOUND).type(MediaType.TEXT_PLAIN).entity(path + " not found").build();
+		}
+
+		try {
+			writeStreamToFile(inputStream, subdir);
+		} catch (IOException e) {
+			return Response.status(Status.NOT_ACCEPTABLE).build();
+		}
+		return Response.ok().build();
+	}
+
+	@Path("{path : .+}")
+	@DELETE
+	public Response deleteFileOrDirectoy(@PathParam("path") String path) {
+		security.assertUserIsAdmin();
+		File workspaceDir = workspace.getWorkspaceDir();
+		File subdir = new File(workspaceDir, path);
+
+		if (!subdir.exists()) {
+			return Response.status(Status.CONFLICT).build();
+		}
+
+		if (subdir.isFile()) {
+			if (subdir.delete()) {
+				return Response.ok().build();
+			}
+			return Response.notModified().build();
+		}
+
+		if (subdir.isDirectory()) {
+			try {
+				FileUtils.deleteDirectory(subdir);
+			} catch (IOException e) {
+				return Response.notModified(e.getMessage()).build();
+			}
+		}
+		return Response.ok().build();
+	}
+
+	private void writeStreamToFile(InputStream in, File resource) throws IOException {
+		FileOutputStream fout = new FileOutputStream(resource);
+		try {
+			final byte buffer[] = new byte[1024];
+			int rc = 0;
+			while (true) {
+				rc = in.read(buffer, 0, buffer.length);
+				if (rc != -1) {
+					fout.write(buffer, 0, rc);
+				} else {
+					break;
+				}
+			}
+
+		} finally {
+			fout.close();
+		}
 	}
 
 	private DirListResult buildDirList(File sourceDirectory) {
