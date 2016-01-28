8edcc1983944180ea4029bdab6b3aa77c58c2e19
Harald Aigner <harald.aigner@arz.at>
2016-01-28 08:02:37 +0100

    (re)move ivy specific classes

--- latte-rodeo/src/at/arz/latte/rodeo/ivy/BaselineRepository.java
+++
@@ -1,9 +0,0 @@
-package at.arz.latte.rodeo.ivy;
-
-import at.arz.latte.rodeo.engine.DependencyNode;
-
-
-public interface BaselineRepository<T> {
-
-	IvyRevision getBaseline(DependencyNode<T> node);
-}
